96d2f71b2dc62f3f7fb660c3194cde3845ad8577
Blankiol <89914536+Blankiol@users.noreply.github.com>
2025-08-11 18:58:31 +0900

    Update_code

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,16 +1,20 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const addcomment = (text) => {
+const addcomment = (text, id) => {
   const videoComments = document.querySelector(".video__comments ul");
   const newComment = document.createElement("li");
+  newComment.dataset.id = id; // Set the data-id attribute to the comment ID
   newComment.className = "video__comment"; // Set the class for the new comment
   const icon = document.createElement("i");
   icon.className = "fas fa-comment";
   const span = document.createElement("span");
   span.innerText = `${text}`; // Set the text content of the span to the comment text
+  const span2 = document.createElement("span");
+  span2.innerText = "❌"; // Create a delete icon
   newComment.appendChild(icon);
   newComment.appendChild(span);
+  newComment.appendChild(span2);
   videoComments.prepend(newComment); // Append the new comment to the list
 };
 
@@ -22,17 +26,18 @@ const handleSubmit = async (event) => {
   if (text === "") {
     return; // If the textarea is empty, do nothing
   }
-  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ text }),
   });
-  textarea.value = ""; // Clear the textarea after submission
 
-  if (status === 201) {
-    addcomment(text);
+  if (response.status === 201) {
+    textarea.value = ""; // Clear the textarea after submission
+    const { newCommentID } = await response.json(); // Parse the JSON response
+    addcomment(text, newCommentID); // Call the function to add the comment
   }
 };
 
